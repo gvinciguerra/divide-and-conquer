467309fbaa3109ec134602b2ccbfe15c3ba13e6f
Giorgio Vinciguerra <i@gvdev.com>
2018-05-23 18:33:32 +0200

    Implemented non-parallel version of D&C

--- include/divideandconquer.h
+++ include/divideandconquer.h
@@ -11,24 +11,40 @@
 
 template<typename TypeIn, typename TypeOut>
 class DivideAndConquer {
+public:
     using is_base_fun_type = std::function<bool(TypeIn &problem)>;
     using divide_fun_type = std::function<std::vector<TypeIn>(TypeIn &problem)>;
     using conquer_fun_type = std::function<TypeOut(TypeIn &problem)>;
-    using combine_fun_type = std::function<TypeOut(std::vector<TypeOut> solutions)>;
+    using combine_fun_type = std::function<TypeOut(std::vector<TypeOut> &solutions)>;
 
-    constexpr is_base_fun_type &is_base;
-    constexpr divide_fun_type &divide;
-    constexpr conquer_fun_type &conquer;
-    constexpr combine_fun_type &combine;
+private:
+    const is_base_fun_type &is_base;
+    const divide_fun_type &divide;
+    const conquer_fun_type &conquer;
+    const combine_fun_type &combine;
     unsigned int parallelism_degree;
-public:
 
+public:
     DivideAndConquer(is_base_fun_type &is_base_fun,
                      divide_fun_type &divide_fun,
                      conquer_fun_type &conquer_fun,
-                     combine_fun_type &combine_fun) : is_base(is_base_fun), divide(divide_fun), conquer(conquer_fun),
-                                                      combine(combine_fun) {
+                     combine_fun_type &combine_fun)
+            : is_base(is_base_fun), divide(divide_fun), conquer(conquer_fun), combine(combine_fun) {
         parallelism_degree = std::thread::hardware_concurrency();
+    }
+
+    TypeOut solve(TypeIn &problem) {
+        if (is_base(problem))
+            return conquer(problem);
+
+        std::vector<TypeIn> subproblems = divide(problem); // FIXME: Copy
+        std::vector<TypeOut> solutions;
+        solutions.reserve(subproblems.size());
+
+        for (auto p : subproblems)
+            solutions.push_back(solve(p));
+
+        return combine(solutions);
     }
 
     void set_parallelism_degree(unsigned int degree) {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include "include/divideandconquer.h"
 
 int main() {
+    std::vector<int> array(100000);
+    std::uniform_int_distribution<int> dist(0, array.size() * 2);
+    std::srand(0);
+    std::generate(array.begin(), array.end(), std::rand);
+
+    using bounds_type = std::pair<std::vector<int>::iterator, std::vector<int>::iterator>;
+    using dc_type = DivideAndConquer<bounds_type, bounds_type>;
+
+    dc_type::is_base_fun_type is_base = [](bounds_type &problem) -> bool {
+        return std::distance(problem.first, problem.second) < 64;
+    };
+
+    dc_type::divide_fun_type divide = [](bounds_type &problem) -> std::vector<bounds_type> {
+        auto mid = problem.first + std::distance(problem.first, problem.second) / 2;
+        std::vector<bounds_type> subproblems;
+        subproblems.reserve(2);
+        subproblems.emplace_back(problem.first, mid);
+        subproblems.emplace_back(mid, problem.second);
+        return subproblems;
+    };
+
+    dc_type::conquer_fun_type conquer = [](bounds_type &problem) -> bounds_type {
+        std::sort(problem.first, problem.second);
+        return problem;
+    };
+
+    dc_type::combine_fun_type combine = [](std::vector<bounds_type> &solutions) -> bounds_type {
+        std::inplace_merge(solutions[0].first, solutions[0].second, solutions[1].second);
+        return std::make_pair(solutions[0].first, solutions[1].second);
+    };
+
+    bounds_type problem = {array.begin(), array.end()};
+    DivideAndConquer<bounds_type, bounds_type> divideAndConquer(is_base, divide, conquer, combine);
+    divideAndConquer.solve(problem);
+    assert(std::is_sorted(array.begin(), array.end()));
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
