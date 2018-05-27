807745e4c7eb82ed275c497d03077d21a16f6553
Giorgio Vinciguerra <i@gvdev.com>
2018-05-27 16:31:48 +0200

    Fixed bug

--- include/divideandconquer.h
+++ include/divideandconquer.h
@@ -8,6 +8,7 @@
 #include <vector>
 #include <functional>
 #include <thread>
+#include "simplepool.h"
 #include "workstealingpool.h"
 
 template<typename TypeIn, typename TypeOut>
@@ -24,7 +25,7 @@ private:
     const conquer_fun_type &conquer;
     const combine_fun_type &combine;
     const size_t parallelism_degree;
-    WorkStealingPool pool;
+    SimplePool pool;
 
 public:
     DivideAndConquer(is_base_fun_type &is_base_fun,
@@ -34,14 +35,15 @@ public:
                      unsigned int parallelism_degree = std::thread::hardware_concurrency())
             : is_base(is_base_fun), divide(divide_fun), conquer(conquer_fun), combine(combine_fun),
               parallelism_degree(parallelism_degree), pool(parallelism_degree) {
-
+        if (parallelism_degree < 1)
+            throw std::invalid_argument("parallelism_degree < 1");
     }
 
     TypeOut solve_sequential(TypeIn &problem) {
         if (is_base(problem))
             return conquer(problem);
 
-        std::vector<TypeIn> subproblems = divide(problem); // FIXME: Copy
+        std::vector<TypeIn> subproblems = divide(problem);
         std::vector<TypeOut> solutions;
         solutions.reserve(subproblems.size());
         for (auto &p : subproblems)
@@ -50,24 +52,27 @@ public:
         return combine(solutions);
     }
 
+    TypeOut solve_helper(TypeIn &problem, size_t remaining_workers) {
+        if (is_base(problem))
+            return conquer(problem);
+        if (remaining_workers <= 1)
+            return solve_sequential(problem);
+
+        std::vector<TypeIn> subproblems = divide(problem);
+        std::vector<std::future<TypeOut>> futures;
+        size_t child_workers = remaining_workers / subproblems.size();
+        for (auto &p : subproblems)
+            futures.emplace_back(pool.submit(&DivideAndConquer::solve_helper, this, p, child_workers));
+
+        std::vector<TypeOut> solutions;
+        for (auto &f : futures)
+            solutions.emplace_back(f.get());
+
+        return combine(solutions);
+    }
+
     std::future<TypeOut> solve(TypeIn &problem) {
-        if (is_base(problem)) {
-            auto task = std::packaged_task<TypeOut()>(
-                    std::bind(conquer, problem)
-            );
-            task.operator()();
-            return task.get_future();
-        }
-
-        auto future = pool.submit(std::this_thread::get_id(), [this, &problem]() {
-            std::vector<TypeIn> subproblems = divide(problem);
-            std::vector<TypeOut> solutions;
-            for (auto &p : subproblems)
-                solutions.push_back(solve_sequential(p));
-            return combine(solutions);
-        });
-
-        return future;
+        return std::move(std::async([this, &problem]() { return solve_helper(problem, parallelism_degree); }));
     }
 };
 
