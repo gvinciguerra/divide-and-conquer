10379f10796267e9564560d4dd55d1c5f11c50e2
Giorgio Vinciguerra <i@gvdev.com>
2018-05-27 18:13:03 +0200

    Added matrix multiplication benchmark

--- main.cpp
+++ main.cpp
@@ -4,7 +4,31 @@
 #include "include/divideandconquer.h"
 #include <benchmark/benchmark.h>
 
-std::vector<int> array(10000000);
+#define data 1000
+std::vector<int> array(data * data);
+std::vector<std::vector<int>> matrix_a(data, std::vector<int>(data));
+std::vector<std::vector<int>> matrix_b(data, std::vector<int>(data));
+
+void BM_merge_sort(benchmark::State &state);
+
+void BM_matrix_multiply(benchmark::State &state);
+
+int main(int argc, char **argv) {
+    std::uniform_int_distribution<int> dist(0, array.size() * 2);
+    std::srand(0); // reproducibility
+    std::generate(array.begin(), array.end(), std::rand);
+    for (auto &r : matrix_a)
+        std::generate(r.begin(), r.end(), std::rand);
+    for (auto &r : matrix_b)
+        std::generate(r.begin(), r.end(), std::rand);
+
+    BENCHMARK(BM_merge_sort)->DenseRange(1, std::thread::hardware_concurrency())->Iterations(1)->UseManualTime();
+    BENCHMARK(BM_matrix_multiply)->DenseRange(1, std::thread::hardware_concurrency())->Iterations(1)->UseManualTime();
+
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+    return 0;
+}
 
 void BM_merge_sort(benchmark::State &state) {
     using bounds_type = std::pair<std::vector<int>::iterator, std::vector<int>::iterator>;
@@ -39,19 +63,65 @@ void BM_merge_sort(benchmark::State &state) {
     bounds_type problem = {copy.begin(), copy.end()};
     DivideAndConquer<bounds_type, bounds_type> divideAndConquer(is_base, divide, conquer, combine, par_degree);
 
+    using namespace std::chrono;
+
     for (auto _ : state) {
-        divideAndConquer.solve(problem).wait();
+        auto t1 = high_resolution_clock::now();
+        divideAndConquer.solve(problem).get();
         // assert(std::is_sorted(array.begin(), array.end()));
+        auto t2 = high_resolution_clock::now();
+        auto time_span = duration_cast<duration<double>>(t2 - t1);
+        state.SetIterationTime(time_span.count());
     }
 }
 
-int main(int argc, char **argv) {
-    std::uniform_int_distribution<int> dist(0, array.size() * 2);
-    std::srand(0); // reproducibility
-    std::generate(array.begin(), array.end(), std::rand);
+void BM_matrix_multiply(benchmark::State &state) {
+    using submatrix_type = struct {
+        size_t ra, rb, ca, cb;
+    };
+    using dc_type = DivideAndConquer<submatrix_type, submatrix_type>;
 
-    BENCHMARK(BM_merge_sort)->DenseRange(1, std::thread::hardware_concurrency(), 1)->Iterations(50);
-    benchmark::Initialize(&argc, argv);
-    benchmark::RunSpecifiedBenchmarks();
-    return 0;
+    std::vector<std::vector<int>> result(data, std::vector<int>(data));
+
+    dc_type::is_base_fun_type is_base = [](submatrix_type &p) -> bool {
+        return p.rb - p.ra < 64;
+    };
+
+    dc_type::divide_fun_type divide = [](submatrix_type &p) -> std::vector<submatrix_type> {
+        std::vector<submatrix_type> subproblems;
+        auto half = (p.rb - p.ra) / 2;
+        subproblems.push_back(submatrix_type{p.ra, p.ra + half, p.ca, p.ca + half});
+        subproblems.push_back(submatrix_type{p.ra, p.ra + half, p.ca + half, p.cb});
+        subproblems.push_back(submatrix_type{p.ra + half, p.rb, p.ca, p.ca + half});
+        subproblems.push_back(submatrix_type{p.ra + half, p.rb, p.ca + half, p.cb});
+        return subproblems;
+    };
+
+    dc_type::conquer_fun_type conquer = [&result](submatrix_type &p) -> submatrix_type {
+        for (size_t i = p.ra; i < p.rb; ++i)
+            for (size_t j = p.ca; j < p.cb; ++j)
+                for (size_t k = p.ra; k < p.rb; ++k)
+                    result[i][j] += matrix_a[i][k] * matrix_b[k][j];
+        return p;
+    };
+
+    dc_type::combine_fun_type combine = [](std::vector<submatrix_type> &solutions) -> submatrix_type {
+        return solutions[0];
+    };
+
+    unsigned int par_degree = state.range(0);
+
+    std::vector<int> copy = array;
+    submatrix_type problem = {0, result.size(), 0, result[0].size()};
+    DivideAndConquer<submatrix_type, submatrix_type> divideAndConquer(is_base, divide, conquer, combine, par_degree);
+
+    using namespace std::chrono;
+
+    for (auto _ : state) {
+        auto t1 = high_resolution_clock::now();
+        divideAndConquer.solve(problem).get();
+        auto t2 = high_resolution_clock::now();
+        auto time_span = duration_cast<duration<double>>(t2 - t1);
+        state.SetIterationTime(time_span.count());
+    }
 }
